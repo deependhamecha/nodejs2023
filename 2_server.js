e91b5206ac56c5776b22916c0f44787a9591a5b3
Deepen Dhamecha <deependhamecha@gmail.com>
2023-03-01 10:04:48 +0530

    Till 33

--- 2_server.js
+++ 2_server.js
@@ -1,7 +1,35 @@
 const http = require('http');
+const fs = require('fs');
 
 function rqListener(req, res) {
-    console.log(req);
+
+    // console.log(req);
+    console.log("Dude");
+
+    if(req.url == '/') {
+        res.write('<html>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message" /><button>Save</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if(req.url == '/message' && req.method == 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
+    // To exit the process
+    // process.exit();
+    
+    res.write('<html>');
+    res.write('<body><h1>Hello World</h1></body>');
+    res.write('</html>');
+    res.end();
+
+    // You can also do this
+    // return res.end();
 }
 
 const server = http.createServer(rqListener);
