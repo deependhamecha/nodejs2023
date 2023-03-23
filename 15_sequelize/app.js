ad9bd5db4cdbc765b9d0834366cf22b633cdbbbf
Deepen Dhamecha <deependhamecha@gmail.com>
2023-03-23 09:32:17 +0530

    Incomplete Sequelize

---
+++ 15_sequelize/app.js
@@ -0,0 +1,86 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
+const sequelize = require('./util/database');
+
+const Product = require('./models/product');
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+
+// db.execute('SELECT * FROM PRODUCTS').then(result => {
+//     console.log('Result: ', result);
+// }).catch(err => {
+//     console.log(err);
+// });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// A middleware we pick User and assign to a user variable
+app.use((req, res, next) => {
+    User.findByPk(1).then(
+        user => {
+            req.user = user;
+            next();
+        }
+    ).catch(err => {
+        console.log(err);
+    });
+})
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+// Associations
+Product.belongsTo(User, {
+    constraints: true,
+    onDelete: 'CASCADE'
+});
+User.hasMany(Product);
+
+
+// Dont use sync({force: true}) on production
+// Replaces table
+sequelize.sync().then(result => {
+
+    // console.log(result);
+
+    return User.findByPk(1);
+
+    
+})
+.then(user => {
+    // Dummy Data
+    if(!user) {
+        return User.create({name: 'Max', email: 'test@test.com'});
+    }
+
+    return user;
+    
+})
+.then(user => {
+    console.log(user);
+    
+    app.listen(3000, () => {
+        console.log('Server started at port 3000');
+    });
+})
+.catch(err => {
+    console.log(err);
+});
+
+
---
+++ 15_sequelize/controllers/admin.js
@@ -0,0 +1,118 @@
+// With Sequelize v5, findById() (which we'll use in this course) was replaced by findByPk().
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+  res.render('admin/add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  
+  Product.create({
+    title,
+    imageUrl,
+    price,
+    description
+  }).then(result => {
+    console.log(result);
+    res.redirect('/admin/products');
+  }).catch(err => {
+    console.log(err);
+  });
+};
+
+exports.getEditProduct = (req, res, next) => {
+  console.log('Coming here');
+  const editMode = req.query.edit;
+  console.log("editMode: ", editMode);
+  if(!editMode) {
+    return res.redirect('/');
+  }
+
+  const prodId = req.params.productId;
+
+  Product.findByPk(prodId).then((product) => {
+    if(!product) {
+      return res.redirect('/');
+    } else {
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true,
+        editing: editMode,
+        product: product
+      });
+      
+    }
+  }).catch(err => {console.log(err)});
+
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+
+  console.log('COMING HERE', prodId);
+
+  Product.findByPk(prodId).then(product => {
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.description = updatedDesc;
+    product.imageUrl = updatedImageUrl;
+    console.log("--------------------------------------------------------");
+    return product.save();
+  })
+  // Returned Promise of save
+  .then(result => {
+    console.log("SAVED PRODUCT: ", result);
+    res.redirect('/admin/products');
+  })
+  .catch(err => {
+    console.log(err);
+  })
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.findAll().then(products => {
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
+    });
+  }).catch(err => {
+    console.log(err);
+  });
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+
+  
+  Product.findByPk(prodId).then(
+    product => {
+      return product.destroy();
+    }
+  )
+  .then(result => {
+    console.log('Product Destroyed');
+    res.redirect('/admin/products');
+  })
+  .catch(err => {
+    console.log(err);
+  });
+}
